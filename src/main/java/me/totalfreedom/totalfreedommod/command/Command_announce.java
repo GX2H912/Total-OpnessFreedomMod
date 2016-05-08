7c3f30d80a675efcba97f431dc6be4bb6d949b51
Mandar Ghade <GX2H912@gmail.com>
2016-05-08 07:34:57 -0400

    Create Command_announce.java

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_announce.java
@@ -0,0 +1,24 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Announce anything for announcments.", usage = "/<command> <message...>", aliases = "csay")
+public class Command_announce extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length > 0)
+        {
+            FUtil.bcastMsg(String.format("'&1[&3&lTotal&5-&8&lOpness&1] &b', sender.getName(), StringUtils.join(args, " ")));
+        }
+        return true;
+    }
+}
